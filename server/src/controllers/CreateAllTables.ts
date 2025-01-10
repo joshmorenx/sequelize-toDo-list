b0837b6be9b906161d5c1d60f1705cb63bb17343
joshmorenx <joshuamoreno21@gmail.com>
2025-01-10 01:56:23 -0600

    feat: deleted comments

--- server/src/controllers/CreateAllTables.ts
+++ server/src/controllers/CreateAllTables.ts
@@ -4,7 +4,6 @@ import { Request, Response } from "express";
 
 export default function CreateAllTables(req: Request, res: Response) {
     try {
-        //create all tables from models if not exists, if exists do nothing
         User.sync();
         Task.sync();
         res.status(200).json({ response: 'All tables created' })    
